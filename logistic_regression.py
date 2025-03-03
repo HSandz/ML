703c60f2fb8270cef45a1dc1219e2964901df26a
Sand <lehoangsand55@gmail.com>
2025-03-03 18:14:07 +0700

    regularized linear and logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,14 +1,22 @@
 import copy, math
 import numpy as np
 import matplotlib.pyplot as plt
+from data.plt_overfit import overfit_example, output
 from data.lab_utils_common2 import dlc, plot_data, plt_tumor_data, sigmoid, compute_cost_logistic
 from data.plt_quad_logistic import plt_quad_logistic, plt_prob
 plt.style.use('./data/deeplearning.mplstyle')
+np.set_printoptions(precision = 8)
 
-def compute_gradient_logistic(X, y, w, b):
+def compute_cost(X, y, w, b, lambda_ = 0):
     m = X.shape[0]
     f_wb = sigmoid(np.dot(X, w) + b)
-    dj_dw = (1 / m) * np.dot(f_wb - y, X)
+    cost = (1 / m) * np.sum((-y) * np.log(f_wb) - (1 - y) * np.log(1 - f_wb)) + (lambda_ / (2 * m)) * np.sum(w ** 2)
+    return cost
+
+def compute_gradient(X, y, w, b, lambda_ = 0):
+    m = X.shape[0]
+    f_wb = sigmoid(np.dot(X, w) + b)
+    dj_dw = (1 / m) * np.dot(f_wb - y, X) + (lambda_ / m) * w
     dj_db = (1 / m) * np.sum(f_wb - y)
     return dj_dw, dj_db
 
@@ -18,51 +26,26 @@ def gradient_descent(X, y, w_in, b_in, alpha, num_iters):
     b = b_in
     
     for i in range(num_iters):
-        dj_dw, dj_db = compute_gradient_logistic(X, y, w, b)   
+        dj_dw, dj_db = compute_gradient(X, y, w, b)   
 
         w = w - alpha * dj_dw               
         b = b - alpha * dj_db               
 
         if i < 100000:
-            J_history.append(compute_cost_logistic(X, y, w, b))
+            J_history.append(compute_cost(X, y, w, b))
 
         if i % math.ceil(num_iters / 10) == 0:
             print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
 
     return w, b, J_history
 
-X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
-y_train = np.array([0, 0, 0, 1, 1, 1])
+np.random.seed(1)
+X_tmp = np.random.rand(5,3)
+y_tmp = np.array([0,1,0,1,0])
+w_tmp = np.random.rand(X_tmp.shape[1])
+b_tmp = 0.5
+lambda_tmp = 0.7
+dj_dw_tmp, dj_db_tmp =  compute_gradient(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
 
-# # Plot data
-# fig,ax = plt.subplots(1,1,figsize=(4,4))
-# plot_data(X_train, y_train, ax)
-#
-# ax.axis([0, 4, 0, 3.5])
-# ax.set_ylabel('$x_1$', fontsize=12)
-# ax.set_xlabel('$x_0$', fontsize=12)
-# plt.show()
-
-w_tmp  = np.zeros_like(X_train[0])
-b_tmp  = 0.
-alph = 0.1
-iters = 10000
-
-w_out, b_out, _ = gradient_descent(X_train, y_train, w_tmp, b_tmp, alph, iters) 
-print(f"\nupdated parameters: w:{w_out}, b:{b_out}")
-
-# Plot the result of gradient descent
-fig,ax = plt.subplots(1,1,figsize=(5,4))
-
-# plot the probability 
-plt_prob(ax, w_out, b_out)
-# Original data
-ax.set_ylabel(r'$x_1$')
-ax.set_xlabel(r'$x_0$')   
-ax.axis([0, 4, 0, 3.5])
-plot_data(X_train,y_train,ax)
-# Decision boundary
-x0 = -b_out/w_out[0]
-x1 = -b_out/w_out[1]
-ax.plot([0,x0],[x1,0], c=dlc["dlblue"], lw=1)
-plt.show()
+print(f"dj_db: {dj_db_tmp}", )
+print(f"Regularized dj_dw:\n {dj_dw_tmp.tolist()}", )
